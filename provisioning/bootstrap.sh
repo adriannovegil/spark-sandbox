8d9a81c49dabcd3936959aae4a1c3666215d919c
Adrian <adrian@localhost.localdomain>
2018-01-13 23:02:20 +0100

    Removing the security configuration. Added the Google DNS servers.

--- provisioning/bootstrap.sh
+++ provisioning/bootstrap.sh
@@ -4,6 +4,24 @@ set -o errexit
 
 echo "Provisioning the Server"
 
+# Config DNS
+sudo cp /vagrant/files/config/resolv.conf /etc/resolv.conf
+
+# Disable security
+# Disable Firewalld In CentOS 7
+sudo systemctl stop firewalld
+sudo systemctl disable firewalld
+# Disable Iptables In CentOS 7
+sudo systemctl stop iptables
+sudo systemctl disable iptables
+# Disable Selinux
+sudo sed -i 's@SELINUX=enforcing@SELINUX=disabled@g' /etc/sysconfig/selinux
+sudo sed -i 's@SELINUX=permissive@SELINUX=disabled@g' /etc/sysconfig/selinux
+sudo sed -i 's@SELINUX=enforcing@SELINUX=disabled@g' /etc/selinux/config
+sudo sed -i 's@SELINUX=permissive@SELINUX=disabled@g' /etc/selinux/config
+sudo setenforce 0
+sudo getenforce
+
 # Install software base
 sudo yum -y install nano wget curl zip bzip2 rpm-build psmisc mvapich2 papi oprofile gnuplot expect bc
 
